52ce9ff41797b392173253b5cdfeba3745519d50
CM <5289021+cm0001@users.noreply.github.com>
2018-11-09 08:48:21 -0600

    Update gdal_retile_multi_N_n.py
    line 1070 - 'xrange' changed to'range' for Python3 compatibility

--- gdal_retile_multi_N_n.py
+++ gdal_retile_multi_N_n.py
@@ -1067,7 +1067,7 @@ def initGlobals():
     
 def split(a, n):
     k, m = divmod(len(a), n)
-    return (a[i * k + min(i, m):(i + 1) * k + min(i + 1, m)] for i in xrange(n))
+    return (a[i * k + min(i, m):(i + 1) * k + min(i + 1, m)] for i in range(n))
 
 #global vars
 Verbose=False
